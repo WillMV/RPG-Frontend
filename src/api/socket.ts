e679535db36a7b6db6dc1edd9e6bcdabd3f6fbb4
Willian M Vargas <104516787+WillMV@users.noreply.github.com>
2026-02-06 14:33:50 -0300

    remove unncessary values in socket url

--- src/api/socket.ts
+++ src/api/socket.ts
@@ -1,10 +1,3 @@
 import { io } from "socket.io-client";
 
-export const socket = io(
-  import.meta.env.VITE_VERCEL_ENV === "preview"
-    ? import.meta.env.VITE_VERCEL_BRANCH_URL.replace("rpg-git-", "").replace(
-        "-willmvs-projects.vercel.app",
-        "",
-      )
-    : import.meta.env.VITE_SOCKET_URL,
-);
+export const socket = io(import.meta.env.VITE_SOCKET_URL);
