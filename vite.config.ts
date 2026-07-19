1b7ac90f2e83e7e95136ed553705e27d9e92f12b
Willian M Vargas <willianm.vargas@gmail.com>
2026-07-19 13:40:57 -0300

    chore: add barrelsby and add auto run in vite plugins

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,28 @@
-import { defineConfig } from "vite";
+import { defineConfig, type ViteDevServer } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import tailwindcss from "@tailwindcss/vite";
 import path from "node:path";
+import { execSync } from "node:child_process";
+
+const regen = () => execSync("npx barrelsby --config .barrelsby.json");
+
+const autoBarrels = {
+  name: "auto-barres",
+  buildStart() {
+    regen();
+  },
+  configureServer(server: ViteDevServer) {
+    server.watcher.on("add", regen);
+    server.watcher.on("unlink", regen);
+  },
+};
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(), autoBarrels],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "src")
-    }
-  }
+      "@": path.resolve(__dirname, "src"),
+    },
+  },
 });
